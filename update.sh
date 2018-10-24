827e9d8483fbb730ab6a2dac8a6b18054c5a1a25
Eduardo Aguilera <eduardo.aguilera@heathmont.net>
2018-10-24 16:56:57 +0300

    commit_message

--- update.sh
+++ update.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+cd ~/dotfiles
+
 cp -u ~/.vimrc ~/dotfiles/vimrc
 
+git add .
+
+echo "What is your vimrc update?"
+read commit_message
+
+git commit -m "commit_message"
+
 git push
+
+echo "Updated"
+
+read ok
