022ec9642992c907309745a7fabf6571a2588ea4
Eduardo Aguilera <laloao.2302@gmail.com>
2021-05-17 16:40:02 +0300

    Adding gitconfig

--- update.sh
+++ update.sh
@@ -2,11 +2,15 @@
 
 cd ~/dotfiles
 
+echo "Adding vimrc"
 cp ~/.vimrc ~/dotfiles/vimrc
+
+echo "Adding .gitconfig"
+cp ~/.gitconfig ~/dotfiles/gitconfig
 
 git add .
 
-echo "What is your vimrc update?"
+echo "What is your update?"
 read commit_message
 
 git commit -m "${commit_message}"
